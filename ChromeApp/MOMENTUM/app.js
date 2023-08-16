dda911e3999f64318025d9f36d2a54860f0f8c8d
rahosung <110014839+rahosung@users.noreply.github.com>
2023-08-16 20:22:39 +0900

    HTML in Javascript

--- ChromeApp/MOMENTUM/app.js
+++ ChromeApp/MOMENTUM/app.js
@@ -1,2 +1,7 @@
-//The Document Object
+// The Document Object
 document.title = "Hello! From JS!";
+
+// HTML in Javascript
+const title = document.getElementById("title");
+
+title.innerText = "Got you!";
