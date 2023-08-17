ca6af067fd99099d42e3991fae6591ff574d2e3b
rahosung <110014839+rahosung@users.noreply.github.com>
2023-08-17 10:21:57 +0900

    Events Part Two

--- ChromeApp/MOMENTUM/app.js
+++ ChromeApp/MOMENTUM/app.js
@@ -1,8 +1,18 @@
-// Events Part One
+// Events Part Two
 const title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
     title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
