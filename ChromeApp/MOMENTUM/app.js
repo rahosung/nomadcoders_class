71508868ee49dd90c0690e0738c6d241fba705b9
rahosung <110014839+rahosung@users.noreply.github.com>
2023-08-22 14:24:37 +0900

    Login - Input Values

--- ChromeApp/MOMENTUM/app.js
+++ ChromeApp/MOMENTUM/app.js
@@ -1,8 +1,10 @@
-// CSS in Javascript Part Three
-const h1 = document.querySelector("div.hello:first-child h1");
+// Input Values
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-    h1.classList.toggle("clicked");
+function onLoginBtnClick() {
+    console.log(loginInput.value);
+    console.log("Click");
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
