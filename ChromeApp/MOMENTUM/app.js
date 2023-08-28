0fc844d9895fe5c565326b2ed7843e6888997c14
rahosung <110014839+rahosung@users.noreply.github.com>
2023-08-28 19:14:28 +0900

    Log In - Events

--- ChromeApp/MOMENTUM/app.js
+++ ChromeApp/MOMENTUM/app.js
@@ -1,9 +1,12 @@
 // Log In
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
+function onLoginSubmit(info) {
+    info.preventDefault();
     const userName = loginInput.value;
-    
+
     console.log(userName);
 }
+
+loginForm.addEventListener("submit", onLoginSubmit);
