11c6342737427467f69884b84d2dff773d4ec76f
rahosung <110014839+rahosung@users.noreply.github.com>
2023-08-29 10:51:08 +0900

    Log In - Getting Username

--- ChromeApp/MOMENTUM/app.js
+++ ChromeApp/MOMENTUM/app.js
@@ -1,20 +1,20 @@
 // Log In
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const userName = loginInput.value;
 
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+
     console.log(userName);
 }
 
-function handleLinkClick(event) {
-    event.preventDefault();
-    alert("clicked");
-}
-
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
