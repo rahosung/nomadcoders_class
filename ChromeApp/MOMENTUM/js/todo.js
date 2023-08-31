5537bac34f1d667a153b2f81b8381f472bdfccb8
rahosung <110014839+rahosung@users.noreply.github.com>
2023-08-31 12:04:02 +0900

    TO DO LIST - Saving ToDos

--- ChromeApp/MOMENTUM/js/todo.js
+++ ChromeApp/MOMENTUM/js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
@@ -27,6 +33,8 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newToDo);
     paintToDo(newToDo)
+    saveTodos();
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
