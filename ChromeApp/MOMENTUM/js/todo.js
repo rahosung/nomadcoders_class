5fb63d0513391e3d53b3b9ec1b1e815a85c399f1
rahosung <110014839+rahosung@users.noreply.github.com>
2023-08-31 09:52:42 +0900

    TO DO LIST - Adding ToDos

--- ChromeApp/MOMENTUM/js/todo.js
+++ ChromeApp/MOMENTUM/js/todo.js
@@ -2,9 +2,18 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newToDo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newToDo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newToDo)
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
