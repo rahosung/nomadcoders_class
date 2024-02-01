37f8eea088adc98f273c020dea05ea72e803a53f
master <master@zioyou.com>
2024-02-01 15:58:15 +0900

    CREATE REACT APP

---
+++ MovieWebService/react-for-beginners/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className={styles.title}>hello</h1>
+      <Button text={"button"}/>
+    </div>
+  );
+}
+
+export default App;
