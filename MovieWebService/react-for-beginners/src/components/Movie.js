5366935ea2f10b83ac7d9a1f2b55ae4fff6de448
master <master@zioyou.com>
2024-02-02 13:08:43 +0900

    React Router

--- MovieWebService/react-for-beginners/src/components/Movie.js
+++ MovieWebService/react-for-beginners/src/components/Movie.js
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({coverImage, title, summary, genres}) {
   return (
     <div>
       <img src={coverImage} />
-      <h2>{title}</h2>
+      <h2><Link to="/movie">{title}</Link></h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
