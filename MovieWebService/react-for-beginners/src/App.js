3fd38623613cef043f4857b47797cf6740335e64
master <master@zioyou.com>
2024-02-02 10:43:04 +0900

    Coin Tracker

--- MovieWebService/react-for-beginners/src/App.js
+++ MovieWebService/react-for-beginners/src/App.js
@@ -1,30 +1,46 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("")
+  // To Do List part One, Two
+  const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
 
-  const onChange = (event) => setToDo(event.target.value)
+  // Coin Tracker
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  
+  // To Do List part One, Two
+  const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
     if (toDo === "") {
       return;
     }
-    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
-  }
-  console.log(toDos);
-  
+  };
+
+  //Coin Tracker
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      <h1 className={styles}>hello</h1>
-      <Button text={"button"}/>
-      
-      <hr />
+      {/* Create React App */}
+      <h1 className={styles}>Create React App</h1>
+      <Button text={"button"} />
 
-      <h1>To Do List part One</h1>
+      <hr style={{ border: "solid 1px" }} />
+      {/* To Do List part One, Two */}
+      <h1>To Do List part One, Two</h1>
       <h2>My To Dos ({toDos.length})</h2>
       <form onSubmit={onSubmit}>
         <input
@@ -42,6 +58,23 @@ function App() {
           <li key={index}>{item}</li>
         ))}
       </ul>
+
+      <hr style={{ border: "solid 1px" }} />
+      {/* Coin Tracker */}
+      <h1>Coin Tracker</h1>
+      <h2>The Coins! {loading ? "" : `(${coins.length})`}</h2>
+      <input></input>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
